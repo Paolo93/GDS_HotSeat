1027c91025cb5beb94fd789a7818992f5ebed386
Pavko <ratalewski@gmail.com>
2021-03-22 21:38:45 +0100

    for designers

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    const string HP_CANVAS = "HP Canvas";
+    Slider slider;
+    Unit unit;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+        unit = GetComponent<Unit>();
+        var canvas = GameObject.FindGameObjectWithTag(HP_CANVAS);
+        if (canvas) transform.SetParent(canvas.transform);
+    }
+
+    private void Update()
+    {
+        if(!unit)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        //slider.value
+    }
+}
