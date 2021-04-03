456837edb3e3bd9119d83e768e9b73464bf8559f
Paolo93 <ratalewski@wp.pl>
2021-04-03 14:07:37 +0200

    clean folders

---
+++ Assets/Scripts/CursorFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CursorFollow : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+    private void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+    }
+}
