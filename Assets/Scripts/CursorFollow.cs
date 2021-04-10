97fbdbd6909da0fe06aa4378c92814fba2d706d6
Pavko <ratalewski@gmail.com>
2021-04-10 18:36:38 +0200

    fix cursor in menu scene

--- Assets/Scripts/CursorFollow.cs
+++ Assets/Scripts/CursorFollow.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CursorFollow : MonoBehaviour
 {
     void Start()
     {
         Cursor.visible = false;
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        if (sceneName == "Menu")
+        {
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.visible = false;
+        }
     }
     private void Update()
     {
